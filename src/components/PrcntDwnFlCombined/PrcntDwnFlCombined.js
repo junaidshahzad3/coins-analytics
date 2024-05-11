29776d4e5e5513ac2e7d9b4202246985415cafb0
junaidquest <junaid.shahzad@questlab.pk>
2024-05-11 20:43:46 +0500

    title fixed

--- src/components/PrcntDwnFlCombined/PrcntDwnFlCombined.js
+++ src/components/PrcntDwnFlCombined/PrcntDwnFlCombined.js
@@ -27,7 +27,7 @@ const PrcntDwnFlCombined = ({
 
   return (
     <div>
-      <div className="text-3xl text-center">Bottoms after Top and when</div>
+      <div className="text-3xl text-center">Bottoms after Top</div>
       <div className="flex items-center gap-3">
         <div className="flex flex-col items-center w-full">
           <CandleStickScatterCmbnd
